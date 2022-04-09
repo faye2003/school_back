e23cc12f686a27aa6f247942dbf57ea89fcf7fc7
faye2003 <fayem7409@gmail.com>
2022-04-09 12:47:31 +0000

    update

---
+++ app/Http/Controllers/ProfesseursController.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class ProfesseursController extends Controller
+{
+    //
+}
--- app/Models/Eleves.php
+++ app/Models/Eleves.php
@@ -8,4 +8,31 @@ use Illuminate\Database\Eloquent\Model;
 class Eleves extends Model
 {
     use HasFactory;
+
+    protected $guarded = [];
+
+    public function inscriptions ()
+    {
+        return $this->belongsTo(Inscriptions::class);
+    }
+
+    public function series ()
+    {
+        return $this->belongsTo(Series::class);
+    }
+
+    public function cours ()
+    {
+        return $this->hasMany(Cours::class);
+    }
+
+    public function disciplines ()
+    {
+        return $this->hasMany(Disciplines::class);
+    }
+
+    public function classes ()
+    {
+        return $this->belongsTo(Classes::class);
+    }
 }
