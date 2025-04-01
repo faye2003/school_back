4fc5c4b3c2670cdad79800d0dd5d3dc8b8b90f02
faye2003 <fayem7409@gmail.com>
2025-04-01 07:26:48 +0200

    first commit

--- resources/views/layouts/app.blade.php
+++
@@ -1,9 +0,0 @@
-<!DOCTYPE html>
-<html lang="fr" ng-app="myApp" ng-controller="MyController as ctrl">
-
-@include('layouts.partials.header')
-
-@yield('content')
-
-@include('layouts.partials.footer')
-</html>
